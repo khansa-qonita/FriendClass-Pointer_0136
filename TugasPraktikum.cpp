0c5f7c00fd5dcde4a357613d667835fc16ba675e
khansa-qonita <khansaqonita420@gmail.com>
2026-04-28 19:34:09 +0700

    implementasi fungsi input data untuk diagonal dan sisi belah ketupat

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -31,3 +31,13 @@ public:
 };
 
 class BelahKetupat {
+    private:
+    double d1, d2, sisi;
+
+public:
+    void input() {
+        cout << "[ Input Belah Ketupat ]" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi: "; cin >> sisi;
+    }
