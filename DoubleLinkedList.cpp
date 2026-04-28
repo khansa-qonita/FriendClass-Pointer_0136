9c3447379f64d795053dc8fad914a06c11ca9c47
khansa-qonita <khansaqonita420@gmail.com>
2026-04-28 18:32:28 +0700

    menambahkan class node

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoublyLinkedList
+{
+private :
+    Node *START;
+
+public :
+    DoublyLinkedList()
+    {
+        
+    }
+}
