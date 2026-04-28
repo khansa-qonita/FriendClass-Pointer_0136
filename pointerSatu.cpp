cf2625abb9ac131e5ede0673457207d4b8aa4651
khansa-qonita <khansaqonita420@gmail.com>
2026-04-28 19:19:36 +0700

    implementasi akses objek melalui reference dan pointer

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,3 +9,14 @@ public:
     }
 };
 
+int main(){
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); //member access operator
+    mahasiswa &refMhs = mhs; //pointer reference refMhs
+    refMhs.nim = 2; //member access operator
+    mhs.showNim();
+    mahasiswa *pMhs = &mhs; //pointer dereference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;
+}
