54646dfc81a83a73c0ad99eb40f51d139bce71af
khansa-qonita <khansaqonita420@gmail.com>
2026-04-28 19:28:06 +0700

    implementasi fungsi input data untuk diagonal dan sisi layang-layang

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -5,3 +5,14 @@ using namespace std;
 
 class BelahKetupat;
 class LayangLayang {
+    private:
+    double d1, d2, s1, s2;
+
+public:
+    void input() {
+        cout << "[ Input Layang-Layang ]" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi Miring 1: "; cin >> s1;
+        cout << "Sisi Miring 2: "; cin >> s2;
+    }
